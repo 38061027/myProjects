7c0c23a1d36982fdcfdaafa4794f873b41f0c910
38061027 <joseaugustogomessantos12@gmail.com>
2024-04-13 19:11:01 -0300

    menu lateral criado

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/services/shared.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  projects:any[] = []
+
+  constructor(private service: SharedService){}
+
+  menuShow(){
+    let menu = document.querySelector('.menu-lateral')
+    if(menu?.classList.contains('open')){
+      menu.classList.remove('open')
+    }else{
+      menu?.classList.add('open')
+    }
+  }
+
+
+  ngOnInit(): void {
+   this.service.getProjects().subscribe(res => this.projects = res)
+  }
 
 }
